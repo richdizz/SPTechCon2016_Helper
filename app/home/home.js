67e4acdc77d15d27505ae2e2418290d4918fb67e
richdizz <richdizz@outlook.com>
2016-02-21 14:21:57 -0600

    Added EazyAuth

--- app/home/home.js
+++ app/home/home.js
@@ -2,27 +2,20 @@
   'use strict';
 
   // The initialize function must be run each time a new page is loaded
-  Office.initialize = function(reason){
-    jQuery(document).ready(function(){
+  Office.initialize = function(reason) {
+    $(document).ready(function(){
       app.initialize();
-      jQuery('#get-data-from-selection').click(getDataFromSelection);
-    });
-  };
-
-  // Reads data from current document selection and displays a notification
-  function getDataFromSelection(){
-    if (Office.context.document.getSelectedDataAsync) {
-      Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-        function(result){
-          if (result.status === Office.AsyncResultStatus.Succeeded) {
-            app.showNotification('The selected text is:', '"' + result.value + '"');
-          } else {
-            app.showNotification('Error:', result.error.message);
-          }
-        }
-        );
-    } else {
-      app.showNotification('Error:', 'Reading selection data not supported by host application.');
-    }
-  }
+      $("#get-data-from-selection").click(function() {
+          AzureADAuth.getAccessToken()
+            .then(function (token) {
+                //handle token
+                $("#token").html(token);
+            })
+            .error(function (err) {
+                //handle error
+                $("#token").html("Error getting token");
+            });
+         });
+      });
+    };
 })();
